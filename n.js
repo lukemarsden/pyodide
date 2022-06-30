fa7208c3805d8d0afa17a2f13d5c909a96008b2e
Luke Marsden <me@lukemarsden.net>
2022-06-30 17:47:42 +0100

    support n.js -c 'print(1+1)', and clean up log output too

--- n.js
+++ n.js
@@ -1,30 +1,87 @@
 async function main() {
+
+    // parse commandline argument -c
+    // set filename to first commandline argument
+
+    var argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+
+    const programPath = null;
+    if (argv._.length == 1) {
+        const programPath = argv._[0];
+    }
+
+    if (programPath === null && !("c" in argv)) {
+        console.log("Must specify a program: 'n.js python.py', or an inline command: 'n.js -c \"print(1+1)\"'")
+        process.exit(1);
+    }
+    
+    if (programPath !== null && ("c" in argv)) {
+        console.log("Can't specify both program path and inline command")
+        process.exit(1);
+    }
+
+    var program = "";
+    if (programPath !== null) {
+        // read contents of file programPath
+        program = await fs.promises.readFile(programPath, 'utf8');
+    }
+
+    if ("c" in argv) {
+        program = argv.c;
+    }
+
+    // set system stdout to dev null
+    // hide "Loading distutils", "Loaded distutils" messages.
+    let oldStdoutWrite = process.stdout.write;
+    process.stdout.write = function() {};
+
     let pyodide_pkg = await import("./pyodide/pyodide.js");
-    let pyodide = await pyodide_pkg.loadPyodide({indexURL: "./pyodide/"});
-    await pyodide.loadPackage("micropip");
+    let LOGGING_ON = false;
+    let pyodide = await pyodide_pkg.loadPyodide({
+      indexURL: "./pyodide/",
+      stdout: (s) => {
+         if (LOGGING_ON) {
+             console.log(s)
+         }
+      },
+      stderr: (s) => {
+         if (LOGGING_ON) {
+             console.log(s)
+         }
+      },
+    });
+    // await pyodide.loadPackage("micropip");
 
-    console.log(await pyodide.runPythonAsync("1+1"));
-    console.log(await pyodide.runPythonAsync(`
-import micropip
-micropip.install("pandas")
-`))
-    console.log(await pyodide.runPythonAsync(`
-import pandas
-from io import StringIO
+    LOGGING_ON = true;
+    process.stdout.write = oldStdoutWrite;
+    await pyodide.runPythonAsync(program);
 
-# CSV String with out headers
-csvString = """Spark,25000,50 Days,2000
-Pandas,20000,35 Days,1000
-Java,15000,,800
-Python,15000,30 Days,500
-PHP,18000,30 Days,800"""
+    // TODO: support requirements
+    // TODO: expose requirements as a webserver, if needed?
 
-# Convert String into StringIO
-csvStringIO = StringIO(csvString)
-import pandas as pd
-df = pd.read_csv(csvStringIO, sep=",", header=None)
-print(df)
-`));
+    //     console.log(await pyodide.runPythonAsync(`
+    // import micropip
+    // micropip.install("pandas")
+    // `))
+
+    //     console.log(await pyodide.runPythonAsync(`
+    // import pandas
+    // from io import StringIO
+
+    // # CSV String with out headers
+    // csvString = """Spark,25000,50 Days,2000
+    // Pandas,20000,35 Days,1000
+    // Java,15000,,800
+    // Python,15000,30 Days,500
+    // PHP,18000,30 Days,800"""
+
+    // # Convert String into StringIO
+    // csvStringIO = StringIO(csvString)
+    // import pandas as pd
+    // df = pd.read_csv(csvStringIO, sep=",", header=None)
+    // print(df)
+    // `));
+
 }
 
 main()
